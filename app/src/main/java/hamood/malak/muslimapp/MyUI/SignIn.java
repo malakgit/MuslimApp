e5c727b9dac9ca7e3dbb9e60ceb9afa9a5de47ed
malakgit <malak8@danon.tzafonet.org.il>
2020-11-30 00:26:40 +0200

    DONE SIGN IN AND SIGN UP

---
+++ app/src/main/java/hamood/malak/muslimapp/MyUI/SignIn.java
@@ -0,0 +1,92 @@
+package hamood.malak.muslimapp.MyUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import hamood.malak.muslimapp.MainActivity;
+import hamood.malak.muslimapp.MyUtils.MyValidations;
+import hamood.malak.muslimapp.R;
+
+public class SignIn extends MainActivity {
+    private Button logIn,signUP;
+    private EditText Password,Email;
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up); //3
+
+        Email=findViewById(R.id.email);
+        Password=findViewById(R.id.password);
+        logIn=findViewById(R.id.logIn);
+        signUP=findViewById(R.id.signUP);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateForm();
+
+            }
+        });
+        signUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(SignIn.this,SignUp.class);
+                startActivity(i);
+            }
+        });
+
+    }
+    //5
+    private void validateForm()
+    {
+        String password=Password.getText().toString();
+        String email=Email.getText().toString();
+        boolean isOk=true;
+        if(email.length()<5 || email.indexOf('@')==0 || email.indexOf('@')>=email.length()-2 ||
+                email.indexOf('.')==0 || email.indexOf('.')>=email.length()-1 || email.lastIndexOf('.')<email.indexOf('@'))
+        {
+            isOk=false;
+            Email.setError("wrong Gmail syntax");
+        }
+        MyValidations myValidations=new MyValidations();
+        if (myValidations.validatePasword(password)==false) {
+            isOk = false;
+            Password.setError("Invalid Password");
+        }
+        if ((isOk)){
+            signIn(email,password);
+        }
+
+
+    }
+
+    private void signIn(String email, String password) {
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent i=new Intent(SignIn.this,MainActivity.class);
+                    startActivity(i);
+                }
+                else
+                {
+                    Toast.makeText(SignIn.this,"FAILED",Toast.LENGTH_SHORT).show();
+                    Email.setError(task.getException().getMessage());
+                }
+            }
+        });
+    }
+
+
+}
