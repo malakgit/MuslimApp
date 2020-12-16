079a6431b1e40815a28cf2ee30bcdcedf55394a5
Owner <alsalamcse>
2020-12-16 08:56:40 +0200

    add the uploadbtn on add_post.xml

--- app/src/main/java/hamood/malak/muslimapp/MyUI/AddPost.java
+++ app/src/main/java/hamood/malak/muslimapp/MyUI/AddPost.java
@@ -1,11 +1,13 @@
 package hamood.malak.muslimapp.MyUI;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -16,6 +18,8 @@ import hamood.malak.muslimapp.MyUtils.MyValidations;
 import hamood.malak.muslimapp.R;
 
 public class AddPost extends AppCompatActivity {
+    private static final int IMAGE_PICK_CODE =1000 ;
+    private static final int PERMISSION_CODE =1001;
     private Button Post,Draft,Code,uploadbtn;
     private ImageButton imagebttn;
     private TextView PostTitle,TextMore;
@@ -57,6 +61,27 @@ public class AddPost extends AppCompatActivity {
         if (isOk){
             MyPost myPost=new MyPost();
         }
+    }
+    public class PickImageExActivity extends AppCompatActivity{
+        private ImageView imageView;
+        private Button chooseimage_btn;
+
+        private static final int IMAGE_PICK_CODE =1000;
+        private static final int PERMISSION_CODE =1001;
+
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.add_post);
+
+            imageView=findViewById(R.id.imageView);
+            chooseimage_btn=findViewById(R.id.chooseimage_btn);
+        }
+    }
+    private void pickImageFromGallery(){
+        //intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
     }
 
 
