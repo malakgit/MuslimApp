1563e37a87fc974e87d3b9f1005577851ad12564
Owner <alsalamcse>
2020-12-21 13:20:21 +0200

    add the uploadbtn on add_post.xml

--- app/src/main/java/hamood/malak/muslimapp/MyUI/MainActivity.java
+++ app/src/main/java/hamood/malak/muslimapp/MyUI/MainActivity.java
@@ -2,6 +2,7 @@ package hamood.malak.muslimapp.MyUI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import hamood.malak.muslimapp.R;
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final android.os.Handler handler = new android.os.Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+
+            }
+        }, 3000);
 
     }
 }
